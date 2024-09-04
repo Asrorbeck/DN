e3456799e5065ce138e92686a79a767cd16feec4
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2024-09-04 21:29:56 +0500

    fourth

--- js/index.js
+++ js/index.js
@@ -1,3 +1,55 @@
+const musicControl = document.getElementById("navbar__btn");
+const audio = document.getElementById("background-music");
+const musicIcon = document.getElementById("music-icon");
+
+let isPlaying = true;
+
+var modal = document.getElementById("myModal");
+var yesBtn = document.getElementById("yesBtn");
+var noBtn = document.getElementById("noBtn");
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+yesBtn.addEventListener("click", function () {
+  audio.muted = false;
+  audio.play();
+  musicIcon.src = "./img/unmute.svg";
+  closeModal();
+
+  isPlaying = !isPlaying;
+});
+
+noBtn.addEventListener("click", function () {
+  audio.pause();
+  musicIcon.src = "./img/mute.svg";
+  closeModal();
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+window.onload = function () {
+  modal.style.display = "block";
+};
+
+musicControl.addEventListener("click", function () {
+  if (isPlaying) {
+    audio.muted = false;
+    audio.play();
+    musicIcon.src = "./img/unmute.svg";
+  } else {
+    audio.pause();
+    musicIcon.src = "./img/mute.svg";
+  }
+
+  isPlaying = !isPlaying;
+});
+
 const targetDate = new Date("2024-09-30T00:00:00").getTime();
 
 const countdown = setInterval(function () {
@@ -22,3 +74,26 @@ const countdown = setInterval(function () {
     document.getElementById("seconds").innerText = seconds;
   }
 }, 1000);
+
+function toggleMenu() {
+  var menu = document.querySelector(".menu");
+  var overlay = document.querySelector(".menu-overlay");
+  menu.classList.toggle("menu-open");
+  overlay.style.display = overlay.style.display === "block" ? "none" : "block";
+}
+
+document.getElementById("openMaps").addEventListener("click", function () {
+  const address = "Яккасарой Туйхонаси";
+
+  if (/iphone|ipad|ipod/i.test(navigator.userAgent)) {
+    // Open Yandex Maps on iOS
+    window.location.href = `https://maps.yandex.com/?text=${encodeURIComponent(
+      address
+    )}`;
+  } else if (/android/i.test(navigator.userAgent)) {
+    // Open Yandex Navigator on Android
+    window.location.href = `https://yandex.com/maps/?text=${encodeURIComponent(
+      address
+    )}`;
+  }
+});
